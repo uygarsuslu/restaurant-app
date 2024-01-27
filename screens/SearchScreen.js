f2c01fea8b8325838887fe47fd939028c3a9048f
Uygar Suslu <uygarsuslu1@gmail.com>
2024-01-28 02:09:38 +0300

    filter restaurants by price

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,15 +1,36 @@
+// @ts-nocheck
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 useResults;
 
 export default function SearchScreen() {
   const [searchApi, results] = useResults();
   console.log(results);
+
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      // bizim göndermiş olduğumuz price'a eşit mi diye kontrol yapıyoruz
+      return result.price === price;
+    });
+  };
   return (
     <View>
       <SearchBar />
+      <ResultsList
+        title="Cheap Restaurants"
+        results={filterResultsByPrice("₺")}
+      />
+      <ResultsList
+        title="Affordable Restaurants"
+        results={filterResultsByPrice("₺₺")}
+      />
+      <ResultsList
+        title="Expensive Restaurants"
+        results={filterResultsByPrice("₺₺₺")}
+      />
     </View>
   );
 }
