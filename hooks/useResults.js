f710fcddc05068552ae116a65f219f0a82b13dbb
Uygar Suslu <uygarsuslu1@gmail.com>
2024-01-28 01:56:29 +0300

    set the return response

--- hooks/useResults.js
+++ hooks/useResults.js
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
+// @ts-nocheck
+import { useEffect, useState } from "react";
 import yelp from "../api/yelp";
 
 // başlangıçta menüsünde tost içeren restoranları çekmek istiyorsak
 export default () => {
+  const [results, setResults] = useState([]);
+
   // Toast = searchTerm'e karşılık gelecek
   const searchApi = async (searchTerm) => {
     // cevap gelene kadar herhangi bir işlem yapmasını istemiyorsak await kullanırız
-    await yelp.get("/search", {
+
+    // burdan dönen cevabı bir değişkene atayabiliriz
+    const response = await yelp.get("/search", {
       params: {
         limit: 50,
         term: searchTerm,
         location: "İstanbul",
       },
     });
+    setResults(response.data.businesses);
   };
 
   useEffect(() => {
@@ -21,5 +27,5 @@ export default () => {
   // içerisinde değişken değiştiğinde çağırılmasını istiyorsak içine değişkeni yazarız
 
   // aramayı yapabilmek için searchApi'yi dışarıya açmamız gerekiyor
-  return [searchApi]; // burdan dönen cevabı bir değişkene atayabiliriz
+  return [searchApi, results];
 };
