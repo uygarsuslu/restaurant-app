8c88fb9ec55cab57f1af3b821a1a059ad6723710
Uygar Suslu <uygarsuslu1@gmail.com>
2024-01-28 01:01:40 +0300

    add search input

---
+++ components/SearchBar.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+
+// npm i @expo/vector-icons
+import { AntDesign } from "@expo/vector-icons";
+
+export default function SearchBar() {
+  return (
+    <View style={styles.backgroundStyle}>
+      <AntDesign
+        style={styles.iconStyle}
+        name="search1"
+        size={30}
+        color="black"
+      />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Ara"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "lightgray",
+    flexDirection: "row",
+    margin: 10,
+    height: 50,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  iconStyle: {
+    marginHorizontal: 15,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+});
