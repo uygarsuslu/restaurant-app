a4737f01715236079fa643947c35408ec13ab03b
Uygar Suslu <uygarsuslu1@gmail.com>
2024-01-28 16:33:16 +0300

    captur restaurant information and display on the screen

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -1,10 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+// @ts-nocheck
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import yelp from "../api/yelp";
 
-export default function ResultsShowScreen() {
+// navigation'da yolladığımız id, route'un içinde tutulur
+export default function ResultsShowScreen({ route }) {
+  // api'den gelen değerleri gösterebilmemiz için
+  const [result, setResult] = useState(null);
+
+  // id'yi değişkene atadık
+  const id = route.params.id;
+
+  // id yerine farklı bir isim de verebilirdik. bu useEffect'in içindeki id'ye karşılık gelir
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  // her seferinde istek atılmasını değil 1 defa istiyorsak [] ile bunu gerçekleştirdik
+  useEffect(() => {
+    //yelp.api'ye istek atmamızı sağlayan method
+    getResult(id);
+  }, []);
+
+  // başlangıçta result gelmeyip, belli bir süre sonra geldiği için bu kodu yazdık
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text>{result.name}</Text>
+      <Text>{result.phone}</Text>
+      <FlatList
+        data={result.photos}
+        renderItem={({ item }) => {
+          return (
+            <Image style={{ width: 50, height: 50 }} source={{ uri: item }} />
+          );
+        }}
+      />
     </View>
   );
 }
