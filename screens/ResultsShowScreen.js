f3bde43b131dd32275f152cc823577e42a0eee73
Uygar Suslu <uygarsuslu1@gmail.com>
2024-01-28 17:33:46 +0300

    add restaurant open-close icon

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -2,6 +2,8 @@
 import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import yelp from "../api/yelp";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 // navigation'da yolladığımız id, route'un içinde tutulur
 export default function ResultsShowScreen({ route }) {
@@ -32,6 +34,14 @@ export default function ResultsShowScreen({ route }) {
     <View>
       <Text style={styles.title}>{result.name}</Text>
       <Text style={styles.phone}>{result.phone}</Text>
+      <View style={styles.icon}>
+        {result.is_closed ? (
+          <AntDesign name="closecircleo" size={30} color="black" />
+        ) : (
+          <MaterialIcons name="delivery-dining" size={30} color="black" />
+        )}
+      </View>
+
       <FlatList
         data={result.photos}
         renderItem={({ item }) => {
@@ -56,5 +66,8 @@ const styles = StyleSheet.create({
   phone: {
     alignSelf: "center",
     fontSize: 20,
+  },
+  icon: {
+    alignSelf: "center",
   },
 });
